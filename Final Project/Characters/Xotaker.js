cf394eadb149d86d02e100dbe4593d3a401e11ea
Your Name <you@example.com>
2018-03-26 21:19:20 +0400

    The 1 Task For The Final Project Was Done

---
+++ Final Project/Characters/Xotaker.js
@@ -0,0 +1,85 @@
+class Xotaker extends KendaniEak {
+    constructor(x, y, index){
+        super(x, y, index);
+        this.tariq = 0;
+    }
+   stanalNorKordinatner() {
+       this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+   }
+   chooseCell(ch) {
+       this.stanalNorKordinatner();
+       return super.yntrelVandak(ch);
+   }
+
+    bazmanal() {
+    if (weatherNum == 0) {
+        if (this.energy >= 10) {
+            xotakerner.push(new Xotaker(this.x, this.y));
+            this.energy = 5;
+        }
+    }
+    else if (weatherNum == 1) {
+        if (this.energy >= 15) {
+            xotakerner.push(new Xotaker(this.x, this.y));
+            this.energy = 5;
+        }
+    }
+    }
+    mahanal() {
+        if (this.energy < 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in xotakerner) {
+                if (this.x == xotakerner[i].x && this.y == xotakerner[i].y) {
+                    xotakerner.splice(i, 1)
+
+                }
+            }
+        }
+    }
+
+    sharjvel() {
+        this.stanalNorKordinatner();
+        var voidCells = this.chooseCell(0);
+        var newCell = random(voidCells);
+        this.energy--;
+
+        if (newCell) {
+            matrix[this.y][this.x] = 0;
+            matrix[newCell[1]][newCell[0]] = 2;
+            this.x = newCell[0];
+            this.y = newCell[1];
+        }
+    }
+
+    utel() {
+        this.stanalNorKordinatner();
+        var voidCells = this.chooseCell(1);
+        var newCell = random(voidCells);
+        if (newCell) {
+            matrix[this.y][this.x] = 0;
+            matrix[newCell[1]][newCell[0]] = 2;
+            this.y = newCell[1];
+            this.x = newCell[0];
+            for (var i in grassArr) {
+                if (grassArr[i].x == this.x && grassArr[i].y == this.y) {
+                    grassArr.splice(i, 1);
+                }
+
+            }
+            this.energy++;
+            this.bazmanal();
+        }
+        else {
+            this.sharjvel();
+        }
+    }
+}
---
+++ Final Project/script.js
@@ -0,0 +1,123 @@
+var matrix = [
+    [0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 9, 0, 0, 0, 0, 0, 3, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 1, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 2, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 9, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 2, 0, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6],
+];
+
+
+var side = 25;
+var grassArr = [];
+var xotakerner = [];
+var gishatichner = [];
+var mah = [];
+var Exanak;
+
+var weatherInfo = document.getElementById("weather");
+var weatherNum = Math.floor(Math.random() * 2);
+    if (weatherNum == 0) {
+        weatherInfo.innerHTML = "Current Weather: Summer ";
+    }
+    else if (weatherNum == 1) {
+        weatherInfo.innerHTML = "Current Weather: Winter ";
+}  
+
+function setup() {
+    frameRate(10);
+    noStroke();
+    createCanvas(matrix[2].length * side, matrix.length * side);
+    background('#acacac');
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                xotakerner.push(new Xotaker(x, y, 2));
+            }
+            else if (matrix[y][x] == 3) {
+                gishatichner.push(new Gishatich(x, y, 3))
+            }
+            else if (matrix[y][x] == 6) {
+                mah.push(new Mah(x, y, 6))
+            }
+        }
+    } 
+}
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1 && weatherNum == 0) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 1 && weatherNum == 1) {
+                fill("white");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("blue");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 6) {
+                fill("black");
+                rect(x * side, y * side, side, side)
+            }
+            else if (matrix[y][x] == 9) {
+                fill("#7b7b8c");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].bazmanal();
+    }
+    for (var i in xotakerner) {
+        xotakerner[i].utel();
+        xotakerner[i].mahanal();
+    }
+    for (var i in gishatichner) {
+        gishatichner[i].utel();
+        gishatichner[i].mahanal();
+    }
+    for (var i in mah) {
+        mah[i].utelXotaker();
+        mah[i].utelGishatich();
+    }
+}
+
